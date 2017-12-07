21ef6227aafb34af7b976365db7945208745cc54
Manuel Mujica <manuel.expat@gmail.com>
2017-12-07 10:33:03 -0700

    Rollback changes made to demo pages

--- graph-visualization/stache-cross-bound/stache-cross-bound.js
+++ graph-visualization/stache-cross-bound/stache-cross-bound.js
@@ -24,7 +24,6 @@ var view = stache(`
 var scope = new Person({ first: "Jane", last: "Doe" });
 
 document.querySelector("#slot").appendChild(view(scope));
-debug.logWhatChangesMe(document.querySelector("#full"));
 draw(
 	document.querySelector("#container"),
 	debug.getGraph(document.querySelector("#full"))
