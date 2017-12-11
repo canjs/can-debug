26ab91934fbb49bf5556607fc9f6f381939d5c85
Manuel Mujica <manuel.expat@gmail.com>
2017-12-11 16:03:07 -0700

    Splig getDebugData in getWhatChangesMe and getWhatIChange
    Closes #22

--- can-debug-test.js
+++ can-debug-test.js
@@ -6,3 +6,6 @@ require("can-debug/src/label-cycles/label-cycles-test");
 require("can-debug/src/log-data/log-data-test");
 require("can-debug/src/what-i-change/what-i-change");
 require("can-debug/src/what-changes-me/what-changes-me-test");
+require("can-debug/src/get-what-i-change/get-what-i-change-test");
+require("can-debug/src/get-what-changes-me/get-what-changes-me-test");
+
