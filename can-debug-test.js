fcbd66e125e59b0237c78099badc2a10994963cc
Matthew Phillips <matthew@matthewphillips.info>
2018-05-02 16:52:56 -0400

    Use typeof to check for Proxy support
    You can't do `Proxy != null` because if Proxy is not defined it will
    cause a ReferenceError. `typeof` is the correct way to test for a
    global.
    
    This also removes the flawed warning test, which depends on nothing else
    having accessed the can namespace before the test runs.

--- can-debug-test.js
+++ can-debug-test.js
@@ -14,23 +14,6 @@ QUnit.test("sets can global namespace", function(assert) {
 	assert.equal(typeof window.can, "object", "should set global namespace");
 });
 
-(Proxy != null ? QUnit.test : QUnit.skip)("warns users accessing global namespace once",
-	function(assert) {
-		var warn = console.warn;
-
-		assert.expect(1);
-		console.warn = function(msg) {
-			assert.ok(/for debugging purposes only/.test(msg));
-		};
-
-		var d = can.debug;
-		d = can.debug;
-		d = can.debug;
-
-		console.warn = warn;
-	}
-);
-
 QUnit.test("sets itself on the global namespace", function(assert) {
 	assert.equal(typeof can.debug, "object", "should set itself");
 });
