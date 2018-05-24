f6578f63d931b3be353f47ce992c8aa1ab3e0c69
Chasen Le Hara <chasen@chasenlehara.com>
2018-05-24 15:27:52 -0700

    1.0.4

---
+++ dist/amd/can-debug.js
@@ -0,0 +1,37 @@
+/*can-debug@1.0.3#can-debug*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-namespace',
+    './src/proxy-namespace',
+    './src/temporarily-bind',
+    './src/get-graph/get-graph',
+    './src/draw-graph/format-graph',
+    './src/draw-graph/draw-graph',
+    './src/what-i-change/what-i-change',
+    './src/what-changes-me/what-changes-me',
+    './src/get-what-i-change/get-what-i-change',
+    './src/get-what-changes-me/get-what-changes-me'
+], function (require, exports, module) {
+    var namespace = require('can-namespace');
+    var proxyNamespace = require('./src/proxy-namespace');
+    var temporarilyBind = require('./src/temporarily-bind');
+    var getGraph = require('./src/get-graph/get-graph');
+    var formatGraph = require('./src/draw-graph/format-graph');
+    var drawGraph = require('./src/draw-graph/draw-graph');
+    var logWhatIChange = require('./src/what-i-change/what-i-change');
+    var logWhatChangesMe = require('./src/what-changes-me/what-changes-me');
+    var getWhatIChange = require('./src/get-what-i-change/get-what-i-change');
+    var getWhatChangesMe = require('./src/get-what-changes-me/get-what-changes-me');
+    module.exports = namespace.debug = {
+        getGraph: temporarilyBind(getGraph),
+        formatGraph: temporarilyBind(formatGraph),
+        drawGraph: temporarilyBind(drawGraph),
+        getWhatIChange: temporarilyBind(getWhatIChange),
+        getWhatChangesMe: temporarilyBind(getWhatChangesMe),
+        logWhatIChange: temporarilyBind(logWhatIChange),
+        logWhatChangesMe: temporarilyBind(logWhatChangesMe)
+    };
+    window.can = typeof Proxy !== 'undefined' ? proxyNamespace(namespace) : namespace;
+});
---
+++ dist/cjs/can-debug.js
@@ -0,0 +1,21 @@
+/*can-debug@1.0.3#can-debug*/
+var namespace = require('can-namespace');
+var proxyNamespace = require('./src/proxy-namespace.js');
+var temporarilyBind = require('./src/temporarily-bind.js');
+var getGraph = require('./src/get-graph/get-graph.js');
+var formatGraph = require('./src/draw-graph/format-graph.js');
+var drawGraph = require('./src/draw-graph/draw-graph.js');
+var logWhatIChange = require('./src/what-i-change/what-i-change.js');
+var logWhatChangesMe = require('./src/what-changes-me/what-changes-me.js');
+var getWhatIChange = require('./src/get-what-i-change/get-what-i-change.js');
+var getWhatChangesMe = require('./src/get-what-changes-me/get-what-changes-me.js');
+module.exports = namespace.debug = {
+    getGraph: temporarilyBind(getGraph),
+    formatGraph: temporarilyBind(formatGraph),
+    drawGraph: temporarilyBind(drawGraph),
+    getWhatIChange: temporarilyBind(getWhatIChange),
+    getWhatChangesMe: temporarilyBind(getWhatChangesMe),
+    logWhatIChange: temporarilyBind(logWhatIChange),
+    logWhatChangesMe: temporarilyBind(logWhatChangesMe)
+};
+window.can = typeof Proxy !== 'undefined' ? proxyNamespace(namespace) : namespace;
