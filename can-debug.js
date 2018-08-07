84a7bea49fc91dd2380ab49a85e186856055a620
Kevin Phillips <kevin@bitovi.com>
2018-08-07 09:14:16 -0500

    removing drawGraph method
    drawGraph loads a large file (vis.js) to do the graph visualization.
    This won't work when using ES Modules or production builds with debug
    enabled, so it is being removed. This same functionality was added to
    CanJS devtools, so that can be used to draw the graph.

--- can-debug.js
+++ can-debug.js
@@ -5,8 +5,7 @@ var proxyNamespace = require("./src/proxy-namespace");
 var temporarilyBind = require("./src/temporarily-bind");
 
 var getGraph = require("./src/get-graph/get-graph");
-var formatGraph = require("./src/draw-graph/format-graph");
-var drawGraph = require("./src/draw-graph/draw-graph");
+var formatGraph = require("./src/format-graph/format-graph");
 var logWhatIChange = require("./src/what-i-change/what-i-change");
 var logWhatChangesMe = require("./src/what-changes-me/what-changes-me");
 var getWhatIChange = require("./src/get-what-i-change/get-what-i-change");
@@ -57,7 +56,6 @@ module.exports = function() {
 	namespace.debug = {
 		getGraph: temporarilyBind(getGraph),
 		formatGraph: temporarilyBind(formatGraph),
-		drawGraph: temporarilyBind(drawGraph),
 		getWhatIChange: temporarilyBind(getWhatIChange),
 		getWhatChangesMe: temporarilyBind(getWhatChangesMe),
 		logWhatIChange: temporarilyBind(logWhatIChange),
